2d659452164721692fd17bdae2675652ddea9a61
Fulvio2909 <fulvio29904@gmail.com>
2025-02-04 11:51:47 +0100

    aggiornare il componente e gli stili della sidebar; integrato il componente nell'app

--- apps/doc-app/src/app/app.tsx
+++ apps/doc-app/src/app/app.tsx
@@ -4,16 +4,13 @@ import { Button } from '@basile-ui/button';
 import { Card } from '@basile-ui/card';
 import { Grid } from '@basile-ui/grid';
 import { Navbar } from '@basile-ui/navbar';
+import { Sidebar } from '@basile-ui/sidebar';
 
 export function App() {
   return (
     <>
-    <Navbar brand='Basile UI' links={[{name: 'Home', href: '/'}, {name: 'About', href: '/about'}]}/>
-      <Grid columns={4} items={[
-        <Card children={<> <p>content card</p> <Button label='button'/> </>} />,
-        <Card children={<> <p>content card</p> <Button label='button'/> </>} />,
-        <Card children={<> <p>content card</p> <Button label='button'/> </>} />
-      ]} />
+      <Navbar brand='Basile UI' links={[{ name: 'Docs', href: '/' }, { name: 'Components', href: '/components' }]} />
+      <Sidebar links={[{ name: 'Button', href: '/button' }, { name: 'card', href: '/card' }]} />
     </>
   );
 }
--- libraries/sidebar/src/lib/sidebar.tsx
+++ libraries/sidebar/src/lib/sidebar.tsx
@@ -1,9 +1,26 @@
+import React from 'react';
 import styles from './sidebar.module.css';
 
-export function Sidebar() {
+type sidebarProps = {
+  links?: { name: string; href: string }[];
+}
+
+export function Sidebar({ links }: sidebarProps) {
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to Sidebar!</h1>
+    <div className={styles.sidebar}>
+      <div className={styles.navbarLinks}>
+        {links && links.map((link, index) => (
+          <>
+            <a href={link.href} className={styles.navbarLink}>
+              {link.name}
+            </a>
+            {index < links.length - 1 && (
+              <span className={styles.divider}></span>
+            )}
+          </>
+        ))}
+
+      </div>
     </div>
   );
 }
