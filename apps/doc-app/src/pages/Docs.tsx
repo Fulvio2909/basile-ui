c8b31142dec42954da6c2bbe5961b77737ae2a8d
Fulvio Dino Basile <150294181+Fulvio2909@users.noreply.github.com>
2025-02-04 15:13:36 +0100

    add routing and layout for Docs and Components pages; update Navbar and Sidebar links (#18)

---
+++ apps/doc-app/src/pages/Docs.tsx
@@ -0,0 +1,19 @@
+import styles from './card.module.css';
+import { Button } from '@basile-ui/button';
+import { Card } from '@basile-ui/card';
+import { Grid } from '@basile-ui/grid';
+import { Navbar } from '@basile-ui/navbar';
+
+type DocsProps = {
+
+}
+
+export function Docs({}: DocsProps) {
+
+  return (
+    <>
+    </>
+  );
+}
+
+export default Docs;
